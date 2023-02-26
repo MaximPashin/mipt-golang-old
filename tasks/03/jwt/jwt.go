49c3d40f8377e0fa56c0de45ff59f100e14055ab
Максим Пашин <pashin@algont.ru>
2023-02-26 22:24:05 +0300

    Refactor Encode

--- tasks/03/jwt/jwt.go
+++ tasks/03/jwt/jwt.go
@@ -29,57 +29,62 @@ var (
 )
 
 func Encode(data interface{}, opts ...Option) ([]byte, error) {
-	conf := config{}
-	for _, opt := range opts {
-		opt(&conf)
+	conf := getConfig(opts...)
+
+	header, err := getHeader(conf)
+	if err != nil {
+		return nil, err
 	}
-	expires, err := getExpires(&conf)
+
+	payload, err := getPayload(&data, conf)
 	if err != nil {
 		return nil, err
 	}
 
 	key := conf.Key
-	signMethod := conf.SignMethod
-	var tokenHashFunc func() hash.Hash
-	switch signMethod {
-	case HS256:
-		tokenHashFunc = sha256.New
-	case HS512:
-		tokenHashFunc = sha512.New
-	default:
-		return nil, ErrInvalidSignMethod
-	}
 
-	header := make(map[string]interface{})
-	header["alg"] = signMethod
-	header["typ"] = "JWT"
-	payload := make(map[string]interface{})
-	payload["d"] = data
-	if expires != nil {
-		payload["exp"] = expires.Unix()
-	}
-
-	encoder := base64.RawURLEncoding
-	var token bytes.Buffer
-
-	marshaledHeader, _ := json.Marshal(header)
-	marshaledPayload, _ := json.Marshal(payload)
-
-	token.WriteString(encoder.EncodeToString(marshaledHeader))
-	token.WriteString(".")
-	token.WriteString(encoder.EncodeToString(marshaledPayload))
-
-	tokenHash := hmac.New(tokenHashFunc, key)
-	tokenHash.Write(token.Bytes())
-	vrfSign := tokenHash.Sum(nil)
-	token.WriteString(".")
-	token.WriteString(encoder.EncodeToString(vrfSign))
-	return token.Bytes(), nil
+	token := buildToken(header, payload, conf.SignMethod, key)
+	return token, nil
 }
 
 func Decode(token []byte, data interface{}, opts ...Option) error {
 	// TODO: implement me
 	return nil
+}
+
+func getConfig(opts ...Option) *config {
+	conf := config{}
+	for _, opt := range opts {
+		opt(&conf)
+	}
+	return &conf
+}
+
+func getHeader(conf *config) (map[string]interface{}, error) {
+	signMethod := conf.SignMethod
+	if signMethod != HS256 && signMethod != HS512 {
+		return nil, ErrInvalidSignMethod
+	}
+	header := map[string]interface{}{
+		"alg": signMethod,
+		"typ": "JWT",
+	}
+	return header, nil
+}
+
+func getPayload(data *interface{}, conf *config) (map[string]interface{}, error) {
+	expires, err := getExpires(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	payload := map[string]interface{}{
+		"d": *data,
+	}
+	if expires != nil {
+		payload["exp"] = expires.Unix()
+	}
+	return payload, nil
 }
 
 func getExpires(conf *config) (*time.Time, error) {
@@ -99,6 +104,38 @@ func getExpires(conf *config) (*time.Time, error) {
 		return nil, ErrConfigurationMalformed
 	}
 	return expires, nil
+}
+
+func buildToken(header map[string]interface{}, payload map[string]interface{}, signMethod SignMethod, key []byte) []byte {
+	encoder := base64.RawURLEncoding
+	var token bytes.Buffer
+
+	marshaledHeader, _ := json.Marshal(header)
+	marshaledPayload, _ := json.Marshal(payload)
+
+	token.WriteString(encoder.EncodeToString(marshaledHeader))
+	token.WriteString(".")
+	token.WriteString(encoder.EncodeToString(marshaledPayload))
+
+	tokenHashFunc := getHashFunc(signMethod)
+
+	tokenHash := hmac.New(tokenHashFunc, key)
+	tokenHash.Write(token.Bytes())
+	vrfSign := tokenHash.Sum(nil)
+	token.WriteString(".")
+	token.WriteString(encoder.EncodeToString(vrfSign))
+	return token.Bytes()
+}
+
+func getHashFunc(signMethod SignMethod) func() hash.Hash {
+	var tokenHashFunc func() hash.Hash
+	switch signMethod {
+	case HS256:
+		tokenHashFunc = sha256.New
+	case HS512:
+		tokenHashFunc = sha512.New
+	}
+	return tokenHashFunc
 }
 
 // To mock time in tests
