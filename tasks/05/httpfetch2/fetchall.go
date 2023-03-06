16b71665040a6bfee60872bd61353ddff1f27136
Максим Пашин <pashin@algont.ru>
2023-03-06 21:51:35 +0300

    Add solution for FetchAll2

--- tasks/05/httpfetch2/fetchall.go
+++ tasks/05/httpfetch2/fetchall.go
@@ -1,8 +1,12 @@
 package httpfetch2
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type Request struct {
@@ -16,8 +20,96 @@ type Result struct {
 	Error      error
 }
 
+var errRedirect = errors.New("Redirect")
+
 func FetchAll(ctx context.Context, c *http.Client, requests <-chan Request) <-chan Result {
-	сh := make(chan Result)
-	close(сh)
-	return сh
+	ch := make(chan Result)
+	var wg sync.WaitGroup
+	go func() {
+		defer func() {
+			wg.Wait()
+			close(ch)
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case request, more := <-requests:
+				if more == true {
+					go func() {
+						wg.Add(1)
+						result := &Result{}
+						httpRequest, err := buildHTTPRequest(&ctx, &request)
+						if err != nil {
+							result = &Result{Error: err}
+						} else {
+							result = fetch(c, httpRequest)
+						}
+						ch <- *result
+						wg.Done()
+					}()
+				} else {
+					return
+				}
+			}
+		}
+	}()
+	return ch
+}
+
+func fetch(c *http.Client, req *http.Request) *Result {
+	select {
+	case <-req.Context().Done():
+		return &Result{Error: context.Canceled}
+	default:
+	}
+	var result *Result
+	response, err := c.Do(req)
+	if err != nil {
+		result = &Result{Error: err}
+	} else {
+		result, err = buildResult(response)
+		if err != nil {
+			switch {
+			case errors.Is(err, errRedirect):
+				result = handleRedirect(c, response)
+			default:
+				result = &Result{Error: err}
+			}
+		}
+	}
+	return result
+}
+
+func buildHTTPRequest(ctx *context.Context, request *Request) (*http.Request, error) {
+	select {
+	case <-(*ctx).Done():
+		return nil, context.Canceled
+	default:
+		return http.NewRequestWithContext(*ctx, request.Method, request.URL, bytes.NewReader(request.Body))
+	}
+}
+
+func buildResult(response *http.Response) (*Result, error) {
+	result := &Result{}
+	result.StatusCode = response.StatusCode
+	if result.StatusCode >= 300 && result.StatusCode < 400 {
+		return nil, errRedirect
+	} else {
+		defer response.Body.Close()
+		if _, err := ioutil.ReadAll(response.Body); err != nil {
+			result.Error = err
+		}
+	}
+	return result, nil
+}
+
+func handleRedirect(c *http.Client, response *http.Response) *Result {
+	redirect := response.Header.Get("Location")
+	newReq, err := http.NewRequest(response.Request.Method, redirect, response.Request.Body)
+	if err != nil {
+		return &Result{Error: err}
+	} else {
+		return fetch(c, newReq)
+	}
 }
