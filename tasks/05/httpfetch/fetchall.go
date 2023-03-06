a914597d37289b62e4a6cd1324a888a9d6e13605
Максим Пашин <pashin@algont.ru>
2023-03-06 16:14:45 +0300

    Add fetchall solution

--- tasks/05/httpfetch/fetchall.go
+++ tasks/05/httpfetch/fetchall.go
@@ -1,7 +1,10 @@
 package httpfetch
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type Request struct {
@@ -16,5 +19,49 @@ type Result struct {
 }
 
 func FetchAll(c *http.Client, requests []Request) []Result {
-	return nil
+	results := make([]Result, len(requests))
+	var group sync.WaitGroup
+	group.Add(len(requests))
+	for i := range requests {
+		go fetchReq(c, &requests[i], &results[i], &group)
+	}
+	group.Wait()
+	return results
+}
+
+func fetchReq(c *http.Client, request *Request, result *Result, group *sync.WaitGroup) {
+	defer group.Done()
+	req, err := http.NewRequest(request.Method, request.URL, bytes.NewReader(request.Body))
+	if err != nil {
+		result.Error = err
+		return
+	}
+	fetchResult := fetch(c, req)
+	result.StatusCode = fetchResult.StatusCode
+	result.Error = fetchResult.Error
+}
+
+func fetch(c *http.Client, req *http.Request) *Result {
+	result := &Result{}
+	response, err := c.Do(req)
+	if err != nil {
+		result.Error = err
+	} else {
+		result.StatusCode = response.StatusCode
+		if result.StatusCode >= 300 && result.StatusCode < 400 {
+			redirect := response.Header.Get("Location")
+			newReq, err := http.NewRequest(req.Method, redirect, req.Body)
+			if err != nil {
+				result.Error = err
+			} else {
+				return fetch(c, newReq)
+			}
+		} else {
+			defer response.Body.Close()
+			if _, err := ioutil.ReadAll(response.Body); err != nil {
+				result.Error = err
+			}
+		}
+	}
+	return result
 }
